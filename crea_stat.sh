74b818db03ab2a0f39563e9fd5925f41f2c9432b
Piero Naldesi <naldesi@dhcp149.bo.infn.it>
2017-08-28 13:23:44 +0200

    modifiche alle

--- crea_stat.sh
+++ crea_stat.sh
@@ -2,15 +2,21 @@
 rm script_wait.sh
 
 printf "%s\n" "#!/bin/bash" >> script_wait.sh
-	
-	
-for L in $(seq 8 2 10)
-	do
 
+LL=($(seq 8 2 16))
+nr=(1 2 3 4 5)
+
+
+len=${#LL[@]%.*} 
+
+for((i=0;i<$len;i++))
+	do
+		L=${LL[i]}
+		
 	for D in $(seq -f "%0.1f" 0.5 0.5 1.0)
 		do
 
-		for n_real in $(seq 1 1 10)
+		for((n_real=0;n_real<${nr[i]};n_real++))
 			do
 		
 		awk -v Gamma=$n_real -v Beta=$D -v Alpha=$L ' 
