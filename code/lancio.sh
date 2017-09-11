089fe6b402626e63979ccc8ee6bd2525492ac022
Piero Naldesi <naldesi@Bologna.local>
2017-09-11 14:50:46 +0200

    uga

--- code/lancio.sh
+++ code/lancio.sh
@@ -4,8 +4,7 @@
 #$ -N L_LLL-D_DDD-nr_nnn
 
 ### file d'attente (a changer)
-#$ -q "E5-2667v2d2deb128,E5-2667v2h6deb128,E5-2670deb128A,E5-2670deb128B,E5-2670deb128C,E5-2670deb128D,E5-2670deb128F,E5-2670deb128nl,E5-2670deb256A,E5-2670deb256C,E5-2670deb256D" 
-###"E5-2667v4deb128nl,E5-2667v4deb256A,E5-2667v2d2deb128,E5-2667v2h6deb128,E5-2670deb128A,E5-2670deb128B,E5-2670deb128C,E5-2670deb128D,E5-2670deb128E,E5-2670deb128F,E5-2670deb128nl"
+#$ -q "E5-2667v2d2deb128,E5-2667v2deb128cssi,E5-2667v2deb128nlspe, E5-2667v2h6deb128,E5-2667v4deb128nl,E5-2670deb128A,E5-2670deb128B,E5-2670deb128C,E5-2670deb128D,E5-2670deb128F,E5-2670deb128nl,E5-2670deb256A,E5-2670deb256C,E5-2670deb256D,E5-2697Av4deb256,x5570deb24A,x5570deb24C,x5570deb24D,x5570deb24E,x5650lin24ibA,x5650lin24ibB" 
 
 ### exporter les variables d'environnement sur tous les noeuds d'execution
 #$ -V
@@ -17,7 +16,6 @@ WORKDIR="/home/pnaldesi/ex_di/code"
 cd ${WORKDIR}
 
 echo $SHELL
-<<<<<<< HEAD
 
 for((i=0; i<20; i++))
 	do
