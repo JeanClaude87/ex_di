dfb1744b212f8c841d2e74e1d97a57341d38f396
Piero Naldesi <naldesi@dhcp149.bo.infn.it>
2017-09-18 12:22:33 +0200

    non medie

--- code/lancio.sh
+++ code/lancio.sh
@@ -6,9 +6,6 @@
 ### file d'attente (a changer)
 ###$ -q "E5-2667v2d2deb128,E5-2667v2deb128cssi,E5-2667v2deb128nlspe,E5-2667v2h6deb128,E5-2667v4deb128nl,E5-2670deb128A,E5-2670deb128B,E5-2670deb128C,E5-2670deb128D,E5-2670deb128F,E5-2670deb128nl,E5-2670deb256A,E5-2670deb256C,E5-2670deb256D,E5-2697Av4deb256,x5570deb24A,x5570deb24C,x5570deb24D,x5570deb24E,x5650lin24ibA,x5650lin24ibB" 
 
-
-#$ -q "E5-2667v2d2deb128,E5-2667v2deb128nlspe,E5-2667v2h6deb128,E5-2667v4deb128nl,E5-2667v4deb256-Impact,E5-2667v4deb256A,E5-2670deb128A,E5-2670deb128B,E5-2670deb128C,E5-2670deb128D,E5-2670deb128E,E5-2670deb128F,E5-2670deb128nl,E5-2670deb256A,E5-2670deb256B,E5-2670deb256C,E5-2670deb256D"
-
 ### exporter les variables d'environnement sur tous les noeuds d'execution
 #$ -V
 
@@ -20,7 +17,7 @@ cd ${WORKDIR}
 
 echo $SHELL
 
-for((i=0; i<2; i++))
+for((i=0; i<5; i++))
 	do
 		
 	python wrap.py LLL DDD nnn 
