76cc684befc664599e0c13ecb7cdf8893f728bb2
Piero Naldesi <naldesi@bo.infn.it>
2017-10-03 12:08:22 +0200

    i

--- code/lancio.sh
+++ code/lancio.sh
@@ -4,7 +4,7 @@
 #$ -N L_LLL-D_DDD-nr_nnn
 
 ### parallel environment & nb cpu (NSLOTS)
-#$ -pe openmp16 16
+###$ -pe openmp16 16
 
 ###$ -q "E5-2670deb128A,E5-2670deb128B,E5-2670deb128C,E5-2670deb128D,E5-2670deb128E,E5-2670deb128F,E5-2667v4deb128nl,E5-2667v4deb256A,E5-2667v2d2deb128,E5-2667v2h6deb128"
 #  $ -q "E5-2667v2d2deb128,E5-2667v2deb128cssi,E5-2667v2deb128nlspe,E5-2667v2h6deb128,E5-2667v4deb128nl,E5-2670deb128A,E5-2670deb128B,E5-2670deb128C,E5-2670deb128D,E5-2670deb128F,E5-2670deb128nl,E5-2670deb256A,E5-2670deb256C,E5-2670deb256D,E5-2697Av4deb256,x5570deb24A,x5570deb24C,x5570deb24D,x5570deb24E,x5650lin24ibA,x5650lin24ibB" 
@@ -24,7 +24,7 @@ for((i=0; i<kkk; i++))
 	do
 	for((j=0; j<50; j++))
 		do		
-	python wrap.py LLL DDD nnn &
+	python wrap.py LLL DDD nnn
 		done
 	done
 
