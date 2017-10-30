23622f30b713fec1278aaa9765ac0f484ce37906
Piero Naldesi <naldesi@lpmmcinvites-22.grenoble.cnrs.fr>
2017-10-30 12:00:59 +0100

    uga

--- script/AA_analizza.py
+++ script/AA_analizza.py
@@ -11,8 +11,8 @@ from scipy.optimize import curve_fit
 
 import AA_functions as ff
 
-names_Corr_files = ['corr_c','corr_H'   ,'corr_H_t'   ,'corr_DE','corr_DE_t','corr_P']
-names_Corr_folds = ['SpSm'  ,'SzSz_Huse','SzSz_Huse_t','SzSz_DE','SzSz_DE_t','SzSz_P']
+names_Corr_files = ['corr_c'] #,'corr_H'   ,'corr_H_t'   ,'corr_DE','corr_DE_t','corr_P']
+names_Corr_folds = ['SpSm'  ] #,'SzSz_Huse','SzSz_Huse_t','SzSz_DE','SzSz_DE_t','SzSz_P']
 
 
 directory = sorted(glob('../dati/*/*/'))
