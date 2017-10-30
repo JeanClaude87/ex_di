b4ec7b4f8b81af2e383df45363eddddc062c1bca
Piero Naldesi <naldesi@lpmmcinvites-22.grenoble.cnrs.fr>
2017-10-30 11:55:36 +0100

    uga

--- script/AA_analizza.py
+++ script/AA_analizza.py
@@ -15,18 +15,15 @@ names_Corr_files = ['corr_c','corr_H'   ,'corr_H_t'   ,'corr_DE','corr_DE_t','co
 names_Corr_folds = ['SpSm'  ,'SzSz_Huse','SzSz_Huse_t','SzSz_DE','SzSz_DE_t','SzSz_P']
 
 
-directory = glob('../dati/*/*/')
-
-print directory
-
+directory = sorted(glob('../dati/*/*/'))
 
 if not os.path.exists("../strange_files/"):
-		os.makedirs("../strange_files/")
+                os.makedirs("../strange_files/")
 
 if not os.path.exists("../plot/dati_fit"):
-	os.makedirs("../plot/dati_fit")
+        os.makedirs("../plot/dati_fit")
 
-print len(directory)
+#print len(directory)
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
@@ -34,12 +31,7 @@ rank = comm.Get_rank()
 
 for i,j in enumerate(directory):
 
-	if i%size!=rank: continue
+        if i%size!=rank: continue
 
-	numCO   = len(names_Corr_files)
-	ff.medie_matrices(j,names_Corr_files,names_Corr_folds,numCO)
-
-
-
-
-
+        numCO   = len(names_Corr_files)
+        ff.medie_matrices(j,names_Corr_files,names_Corr_folds,numCO)
