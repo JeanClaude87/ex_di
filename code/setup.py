d0c22f07d5bd60be0b04eb8b7a92af73735576b4
Piero Naldesi <naldesi@bo.infn.it>
2018-05-30 13:51:06 +0200

    uga

--- code/setup.py
+++ code/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-	ext_modules = cythonize('f*.py')
+	ext_modules = cythonize('f_function.py')
 )
 
 # python setup.py build_ext --inplace
