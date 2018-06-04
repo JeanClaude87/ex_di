38c978b21a58f55b58b9c33df6bbb8801513482c
Piero Claude <naldesi@bo.infn.it>
2018-06-04 14:19:14 +0200

    EVOLUZIONE TEMP

--- crea_stat.sh
+++ crea_stat.sh
@@ -10,9 +10,9 @@ cd code
 # rm -rf *so *c *build *core __pycache__
 cd ..
 
-printf "%s\n" "#!/bin/bash" >> script_wait1.sh
+printf "%s\n" "#!/bin/bash" >> script_wait.sh
 
-LL=(8 10 12 14 16)
+LL=(12)
 #DD=(1)   
 #DD=(1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0)
 #DD=(0.5 1.5 2.5 3.5 4.5 5.5 6.5 7.5 8.5)
@@ -24,7 +24,7 @@ LL=(8 10 12 14 16)
 DD=(2. 5.)
 
 #nr=(1 1 1 1 1) 
-nr=(100 80 60 50 30) #*20
+nr=(50) #*20
 ncy=1
 
 lenl=${#LL[@]} 
@@ -47,13 +47,13 @@ for((i=0; i<$lenl; i++))
 
                         mv temp.tmp uga-L_$L-D_$D-nr_$n_real.inp        
 
-                        printf  "%s\n" "qsub uga-L_$L-D_$D-nr_$n_real.inp" >> script_wait1.sh
+                        printf  "%s\n" "qsub uga-L_$L-D_$D-nr_$n_real.inp" >> script_wait.sh
 
                         done
                 done
         done    
 
-chmod +x script_wait1.sh
+chmod +x script_wait.sh
 
 #./script_wait.sh
 
