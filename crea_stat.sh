9f209e357f5b89590457e175915c5ec1db38d655
Piero Claude <naldesi@bo.infn.it>
2018-06-05 16:27:20 +0200

    EVOLUZIONE TEMP

--- crea_stat.sh
+++ crea_stat.sh
@@ -1,30 +1,20 @@
-#rm -f script_wait.sh
-#rm -f u*
+rm -f script_wait.sh
+rm -f u*
 rm -f ../L*
 
-
-
 cd code
-       python setup.py build_ext --inplace
+    python3 setup.py build_ext --inplace
     wait    
-# rm -rf *so *c *build *core __pycache__
 cd ..
 
 printf "%s\n" "#!/bin/bash" >> script_wait.sh
 
 LL=(8)
-#DD=(1)   
-#DD=(1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0)
-#DD=(0.5 1.5 2.5 3.5 4.5 5.5 6.5 7.5 8.5)
-
 
 #DD=(2. 2.25 2.5 2.75 3. 3.25 3.5 3.75 4. 4.25 4.5 4.75 5.)
-#5.25 5.5 5.75 6. 6.5 7. 7.5 8. 8.5 9.)
-
 DD=(2.)
 
-#nr=(1 1 1 1 1) 
-nr=(1) #*16
+nr=(1)
 ncy=1
 
 lenl=${#LL[@]} 
@@ -44,7 +34,6 @@ for((i=0; i<$lenl; i++))
                         do
                 
                         sed -e "s/kkk/$ncy/g" -e "s/LLL/$L/g" -e "s/DDD/$D/g" -e "s/nnn/$n_real/g" < code/lancio.sh > temp.tmp
-
                         mv temp.tmp uga-L_$L-D_$D-nr_$n_real.inp        
 
                         printf  "%s\n" "qsub uga-L_$L-D_$D-nr_$n_real.inp" >> script_wait.sh
@@ -54,7 +43,6 @@ for((i=0; i<$lenl; i++))
         done    
 
 chmod +x script_wait.sh
-
 #./script_wait.sh
 
 
